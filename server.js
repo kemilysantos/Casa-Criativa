be6b70a3014ca7c638cb999fa1c80d5b9ce4214a
kemilysantos <kemilysantos779@gmail.com>
2020-07-22 15:39:02 -0300

    iniciando banco de dados

--- server.js
+++ server.js
@@ -1,59 +1,60 @@
-//usei o express para criar e configurar meu servidor
+
 const express = require('express')
 const server = express()
 
 
+const db = require("./db")
+//const ideas = [
+    //{   
+        //img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        //title: "Cursos de Programação",
+        //category:"Estudo",
+        //description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
+        //url: "https://rocketseat.com.br",
 
-const ideas = [
-{   
-     img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    title: "Cursos de Programação",
-    category:"Estudo",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
-    url: "https://rocketseat.com.br",
+    //},
 
-    },
+    //{   
+        //img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        //title: "Imagem de Exercício",
+        //category:"Saúde",
+        //description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
+        //url: "https://rocketseat.com.br",
+    //},
 
-    {   
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Imagem de Exercício",
-        category:"Saúde",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
-        url: "https://rocketseat.com.br",
-    },
+    //{
+        //img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        //title: "Meditação",
+        //category:"Mentalidade",
+        //description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
+        //url: "https://rocketseat.com.br",
+    //},
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category:"Mentalidade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
-        url: "https://rocketseat.com.br",
-    },
+    //{
+        //img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        //title: "Karaokê",
+        //category:"Diversão em Família",
+        //description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
+        //url: "https://rocketseat.com.br",
+    //},
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category:"Diversão em Família",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
-        url: "https://rocketseat.com.br",
-    },
+    //{
+        //img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
+        //title: "Pintura",
+        //category:"Criatividade",
+        //description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
+        //url: "https://rocketseat.com.br",
+    //},
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-        title: "Pintura",
-        category:"Criatividade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
-        url: "https://rocketseat.com.br",
-    },
+    //{
+        //img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
+        //title: "Recortes",
+        //category:"Criatividade",
+        //description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
+        //url: "https://rocketseat.com.br",
+    //},
 
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-        title: "Recortes",
-        category:"Criatividade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nisi alias ipsum, fugiat sed",
-        url: "https://rocketseat.com.br",
-    },
-]
+//]
 
 //configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
@@ -68,25 +69,37 @@ nunjucks.configure("views", {
 //criei uma rota/
 //e capturo o pedido do cliente para responder
 server.get("/", function(req, res){
-
-    const reversedIdeas = [...ideas].reverse()
-
-    let lastIdeas =[]
-    for(let idea of ideas){
-        if(lastIdeas.length <2){
-            lastIdeas.push(idea)
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados")
         }
-    }
-    
 
-    return res.render("index.html", { ideas:lastIdeas })
+       const reversedIdeas = [...rows].reverse()
+
+        let lastIdeas =[]
+        for(let idea of reversedIdeas){
+            if(lastIdeas.length < 2){
+                lastIdeas.push(idea)
+            }
+        }
+        return res.render("index.html", { ideas:lastIdeas })
+    })
 })
 
 server.get("/ideias", function(req, res){
-    const reversedIdeas = [...ideas].reverse()
-    return res.render("ideias.html", { ideas: reversedIdeas})
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados")
+        }
+
+        const reversedIdeas = [...rows].reverse()
+    
+        return res.render("ideias.html", { ideas: reversedIdeas})
+    })
 })
 
 // liguei meu servidor na porta 3000
-server.listen(3000)
+server.listen(3000)//
 
